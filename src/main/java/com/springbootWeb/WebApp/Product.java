2b9f620f1f4e4cf98ab443d321b9a32f01ed6334
Akash Sharma <akash.sharma@arrise.com>
2026-07-14 11:08:10 +0530

    Added product and productcontroller class

---
+++ src/main/java/com/springbootWeb/WebApp/Product.java
@@ -0,0 +1,11 @@
+package com.springbootWeb.WebApp;
+
+public class Product {
+
+    private int prodId;
+    private String prodName;
+    private int price;
+
+
+}
+
---
+++ src/main/java/com/springbootWeb/WebApp/ProductController.java
@@ -0,0 +1,9 @@
+package com.springbootWeb.WebApp;
+
+public class ProductController {
+
+    public String getProducts(){
+        return "";
+    }
+
+}
